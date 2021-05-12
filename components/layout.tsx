6230348c3250463e87bdc0db8afcbc9f015c281d
Oscar Marti <kurtogath@gmail.com>
2021-05-12 21:48:31 +0200

    Removed CSS add react icons

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,21 @@
 import { Box, Flex } from '@chakra-ui/layout';
-import { Button } from '@chakra-ui/button';
+import { IconButton } from '@chakra-ui/button';
 import { useColorMode } from '@chakra-ui/color-mode';
+import { FiSun, FiMoon } from 'react-icons/fi';
 
 const Layout: React.FC = ({ children }) => {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
         <Flex height='100vh' width='100vw'>
             <Box w='250px' h='100vh' shadow='xl' p={[2, 3, 5]}>
-                <Button onClick={toggleColorMode}>Toggle {colorMode === 'light' ? 'Dark' : 'Light'}</Button>
+                <IconButton
+                    aria-label='Toggle color mode'
+                    onClick={toggleColorMode}
+                    icon={colorMode === 'light' ? <FiSun /> : <FiMoon />}
+                    bg='transparent'
+                />
             </Box>
-            <Box w='100%' h='100vh' bg='whiteAlpha.300' p={[2, 3, 5]}>
+            <Box w='100%' h='100vh' bg='whitesmoke' p={[2, 3, 5]}>
                 {children}
             </Box>
         </Flex>
